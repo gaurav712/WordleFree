a3adac842bba5831fef6d85a1959f07779f1b888
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2023-02-24 01:32:38 +0530

    remove source link references

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet, View} from 'react-native';
-import SourceLink from './app/components/SourceLink';
 import ScreenHeader from './app/components/ScreenHeader';
 import GameScreen from './app/GameScreen';
 
@@ -10,7 +9,6 @@ export default function App() {
       <SafeAreaView style={styles.container}>
         <ScreenHeader />
         <GameScreen />
-        <SourceLink />
       </SafeAreaView>
     </View>
   );
